2e3522bde192ae9b0b292f157694fc3a5b5a8a22
Conrad Hope <conjhope@outlook.com>
2023-09-16 11:33:04 -0600

    9/16 commit

--- week-14-assignment-app/src/App.js
+++ week-14-assignment-app/src/App.js
@@ -1,20 +1,46 @@
-import MovieList from './Assets/MovieList'
+import MovieList from './Assets/MovieList';
 import ReviewList from './Assets/ReviewList';
 import ReviewForm from './Assets/ReviewForm';
-import Movie from './Components/Movie';
+import {TEST_MOVIE} from './Components/Movie';
 import Stars from './Components/Stars';
 import Review from './Components/Review';
 import './App.css';
+import {useState} from "react";
+
+let nextId = 2;
 
 function App() {
+  const [reviewList, setReviewList] = useState(TEST_MOVIE)
+
+  const incrementStars = (id) => {
+    setReviewList(reviewList.map(review =>
+      (review.id === id) ?
+       {...review, quantity: review.quantity + 1} :
+        review
+      ))
+  }
+
+  const addReview = (reviewData) => {
+    const newReview = { id: nextId++, ...reviewData}
+
+    setReviewList (reviewList.conact(newReview))
+  }
+
   return (
     <div className="App">
-      <MovieList />
-      <ReviewList />
-      <ReviewForm />
-      <Movie />
-      <Stars />
-      <Review />
+      <h1 className="appHeader"> Rate That Movie</h1>
+      <ReviewForm addReview={addReview}/>
+        {reviewList.map(review =>
+          <div key={review.id}>
+            {review.name} - {review.quantity}
+            <button onClick={() => incrementStars(review.id)}>Add Stars</button>
+          </div>
+            )}
+      {/* <Movie /> */}
+      {/* <MovieList />
+      < ReviewList/> */}
+      {/* <Stars />
+      <Review /> */}
     </div>
   );
 }
